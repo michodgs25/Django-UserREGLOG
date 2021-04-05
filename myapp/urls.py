133e542659fd906d5e60286c89a8d4a3bfb0478a
michodgs25 <michodgs@gmail.com>
2021-04-05 20:54:58 +0000

    Add custom urls

---
+++ myapp/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('myapp.views',
+                       url(r'^hello/', 'hello', name='hello'),
+                       url(r'^morning/', 'morning', name='morning'),)
--- spacekeys/urls.py
+++ spacekeys/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.urls import path
+admin.autodiscover()
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+                       # Examples
+                       # url(r'^$', 'myproject.view.home', name = 'home'),
+                       # url(r'^blog/', include('blog.urls')),
+
+                       url(r'^admin', include(admin.site.urls)),
+                       url(r'^myapp/', include('myapp.urls')),
+)
