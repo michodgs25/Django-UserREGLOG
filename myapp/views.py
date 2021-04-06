ab5795997f299cb240c41afdf668528839085278
michodgs25 <michodgs@gmail.com>
2021-04-06 15:11:10 +0000

    Add login view

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from myapp.forms import LoginForm
 
 # Create your views here.
+
+
+def login(request):
+    username = "not logged in"
+
+    if request.method == "POST":
+        MyLoginForm = LoginForm(request.POST)
+
+    if MyLoginForm.is_valid():
+        username = MyLoginForm.cleaned_data['username']
+    else:
+        MyLoginForm = LoginForm()
+
+        return render(request, 'loggedin.html', {"username": username})
