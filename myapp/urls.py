1dcc8a037b8ea8f9efb436217197c40146a3b1e0
michodgs25 <michodgs@gmail.com>
2021-04-06 20:34:28 +0000

    Add new dependencies

--- myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import (
-    include, url,
-    TemplateView)
-
-urlpatterns = ('myapp.views',
-               url(r'^connection/',
-                   TemplateView.as_view(template_name='login.html')),
-               url(r'^login/', 'login', name='login'))
