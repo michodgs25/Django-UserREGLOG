39d3dc8ae35e01b410e436b5dd844995ea3b8c3e
michodgs25 <michodgs@gmail.com>
2021-04-06 10:33:39 +0000

    Install crispy forms

--- spacekeys/settings.py
+++ spacekeys/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path, os
 import dj_database_url
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
