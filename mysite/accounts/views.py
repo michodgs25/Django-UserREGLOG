fdd62cb1a6f8ec30269779419e6c2f228413f79f
michodgs25 <michodgs@gmail.com>
2021-04-09 15:26:57 +0000

    Install additional dependencies

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, user
 # Create your views here.
+
+if user is not None:
+    # A backend authenticated the credentials
+    else:
+        # No backend authenticated the credentials
