4639e19df186f3518705a939825ab902dd3f2c70
michodgs25 <michodgs@gmail.com>
2021-04-12 09:39:32 +0000

    Add register form view

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, user
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-if user is not None:
-    # A backend authenticated the credentials
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
     else:
-        # No backend authenticated the credentials
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
